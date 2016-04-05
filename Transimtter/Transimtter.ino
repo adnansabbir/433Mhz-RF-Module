17ff6dbf74e32fa885d0a0c219b3857eee589226
M M Shaifur Rahman <shaifur.cse@gmail.com>
2016-04-06 01:24:44 +0600

    update v2
    Delete unnecessary information from previous version

--- Transimtter/Transimtter.ino
+++ Transimtter/Transimtter.ino
@@ -1,33 +1,20 @@
 #include <VirtualWire.h>
-
-const int led_pin = 13;
 const int transmit_pin = 8;
-const int receive_pin = 2;
-const int transmit_en_pin = 3;
-
 void setup()
 {
   // Initialise the IO and ISR
   vw_set_tx_pin(transmit_pin);
-  vw_set_rx_pin(receive_pin);
-  vw_set_ptt_pin(transmit_en_pin);
-  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_set_ptt_inverted(true); // Required
   vw_setup(2000);   // Bits per sec
   Serial.begin(9600);
 }
-
 byte count = 1;
-
 void loop()
 {
   char msg[2] = {'A', 'B'};
-
-  //msg[1] = count;
-  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
   vw_send((uint8_t *)msg, 2);
   vw_wait_tx(); // Wait until the whole message is gone
-  digitalWrite(led_pin, LOW);
+  Serial.println("Send");
   delay(1000);
   count = count + 1;
-  Serial.println("Send");
 }
